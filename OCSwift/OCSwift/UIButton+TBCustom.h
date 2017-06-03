c0f330a408c64525a800eb878e4e6f1b709f233b
田彬彬 <tbb@tianbinbindeMac-mini.local>
2017-06-03 13:35:26 +0800

    参考了网上的大牛们的杰作

---
+++ OCSwift/OCSwift/UIButton+TBCustom.h
@@ -0,0 +1,13 @@
+//
+//  UIButton+TBCustom.h
+//  OC_Swift 防止UIButton重复点击
+//
+//  Created by 田彬彬 on 2017/6/2.
+//  Copyright © 2017年 田彬彬. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIButton (TBCustom)
+
+@end
